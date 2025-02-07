b52613a9a6ac1945915a7bbb5a32ab70fdbac15e
Jaimi Patel <jaimip.albiorix@gmail.com>
2025-02-07 13:49:54 +0530

    Create sign up page UI

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,9 +4,14 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
+      path: '/login',
       name: 'Login',
       component: () => import('@/views/LogIn.vue'),
+    },
+    {
+      path: '/signup',
+      name: 'Signup',
+      component: () => import('@/views/SignUp.vue'),
     },
   ],
 })
--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -4,21 +4,22 @@ import { RouterLink } from 'vue-router'
 import { ref } from 'vue'
 
 const isPassword: boolean = ref(false)
-const login = () => {
+const isCPassword: boolean = ref(false)
+const signup = () => {
   console.log('Hello')
 }
 </script>
 
 <template>
   <div class="container mx-auto my-10 flex justify-center space-x-10" style="height: 80vh">
-    <div class="shadow-2xl p-10 m-10 rounded rounded-3xl">
-      <h2 class="text-5xl text-gray-900 font-bold">Login</h2>
+    <div class="shadow-2xl py-6 my-4 px-10 mx-10 rounded rounded-3xl w-full">
+      <h2 class="text-5xl text-gray-900 font-bold">Sign Up</h2>
       <p class="text-gray-500 font-semibold text-lg my-2">
-        Doesn't have an account yet?
-        <RouterLink to="/signup" class="underline text-blue-700">Sign Up</RouterLink>
+        Already have an account?
+        <RouterLink to="/login" class="underline text-blue-700">Login</RouterLink>
       </p>
 
-      <form @submit.prevent="login">
+      <form @submit.prevent="signup">
         <div>
           <label for="email" class="label">Email Address</label>
           <input type="text" id="email" class="input" placeholder="Enter Your Email Address" />
@@ -40,18 +41,29 @@ const login = () => {
           </div>
         </div>
 
-        <div class="flex items-center space-x-3 my-10 text-lg text-gray-500 font-semibold">
-          <input type="checkbox" id="remember" class="w-6 h-6 cursor-pointer" />
-          <label for="remember">Remember Me</label><br />
+        <div>
+          <label for="cPassword" class="label">Confirm Password</label>
+          <div class="input flex items-center justify-between">
+            <input
+              :type="isCPassword ? 'text' : 'password'"
+              id="cPassword"
+              class="text-gray-900 placeholder:text-gray-400 focus:outline-none text-xl"
+              placeholder="Enter Your Confirm Password"
+            />
+            <div class="pl-2" @click="isCPassword = !isCPassword">
+              <EyeSlashIcon v-if="isCPassword" class="password-icon" />
+              <EyeIcon v-else class="password-icon" />
+            </div>
+          </div>
         </div>
 
         <button
-          class="bg-blue-700 text-2xl font-bold text-white p-5 w-full rounded rounded-lg hover:bg-blue-600 disabled:bg-blue-400 disabled:cursor-not-allowed"
+          class="mt-10 bg-blue-700 text-2xl font-bold text-white p-5 w-full rounded rounded-lg hover:bg-blue-600 disabled:bg-blue-400 disabled:cursor-not-allowed"
         >
-          Login
+          Sign Up
         </button>
       </form>
     </div>
-    <img src="@/assets/img/login.png" alt="Login" />
+    <img src="@/assets/img/signup.png" alt="Login" />
   </div>
 </template>
